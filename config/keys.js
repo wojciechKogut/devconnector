ee320c50b9e3c8a994cd38505d3e039750c36b1f
wojciechKogut <wkgt33@gmail.com>
2018-07-28 19:16:56 +0200

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://wojtek:wojtek1337@ds259111.mlab.com:59111/devconnector1'
+}
---
+++ routes/api/users.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+//@route  GET api/posts/test
+//@desc   Test post route
+//@access Public
+//example
+
+module.exports = router;
